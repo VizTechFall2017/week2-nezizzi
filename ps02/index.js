dcdb5eabf7ff74ec22998e5743e35f1ddeeca4ca
nezizzi <zizzi.n@husky.neu.edu>
2017-09-24 14:45:41 -0400

    a start

--- ps02/index.js
+++ ps02/index.js
@@ -1,3 +1,20 @@
-var svg = d3.select('svg');
+//var svg = d3.select('svg');
 
-/* Your code goes here */
+var BODY = d3.select('body').append('svg')
+    .attr('width',800)
+    .attr('height',800);
+
+
+
+var circle = BODY.append('circle')
+    .attr('cx',600)
+    .attr('cy',100)
+    .attr('r',60)
+    .attr('fill', 'purple')
+    .attr('stroke-width', '2%')
+    .attr('stroke', '#0f5a80');
+
+var BOX = BODY.append('rectangle')
+    .attr('x', 150)
+    .attr('y', 0)
+    .attr('width',50);
