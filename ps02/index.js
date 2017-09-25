8397beaffda95d48066f9e75fc87eb6a5ea27151
nezizzi <zizzi.n@husky.neu.edu>
2017-09-24 20:19:03 -0400

    a new start

--- ps02/index.js
+++ ps02/index.js
@@ -13,49 +13,22 @@ var circleA = BODY.append('circle')
     .attr('stroke', '#0f5a80');
 
 
-
 var center = 1;
 rectnumber_center = 11;
 
-for ( i = 0; i < rectnumber_center; i++){
+for ( i = 0; i < rectnumber_center; i++) {
     var x = 0;
     var y = -50;
-    console.log(center);
-        if (center = 1){
-            //rectA
-            x = x +50;
-            y= y + 50;
-            console.log(x);
-            console.log(y);
-            RECT = BODY.append('rect')
-            .attr('x', x)
-            .attr('y', y)
-            .attr('width',50)
-            .attr('height',50)
-            .attr('fill', 'green');
-            center = center +1;}
-        else if (center = 2){
-            //rectB
-            RECT = BODY.append('rect');
-            x = x +50;
-            y= y + 50
-            .attr('x', x)
-            .attr('y', y)
-            .attr('width',50)
-            .attr('height',50)
-            .attr('fill', '#53800c')
-            .attr('stroke-width', '1%');
-                center= center+1;}
-        else {
-        //rectC
-            RECT = BODY.append('rect');
-            x = x +50;
-            y= y + 50
-            .attr('x', x)
-            .attr('y', y)
-            .attr('width',50)
-            .attr('height',50)
-            .attr('fill', '#178067;')
-            .attr('stroke-width', '1%');
-        center = 1;}
+    //rectA
+    x = x + 50;
+    y = y + 50;
+    console.log(x);
+    console.log(y);
+    RECT = BODY.append('rect')
+        .attr('x', x)
+        .attr('y', y)
+        .attr('width', 50)
+        .attr('height', 50)
+        .attr('fill', 'green');
 }
+          
