f1fa0d4686d17c70b507c01a576ef806d872d6f8
nezizzi <zizzi.n@husky.neu.edu>
2017-09-24 20:44:17 -0400

    trying to get colors to work in rectangles

--- ps02/index.js
+++ ps02/index.js
@@ -13,7 +13,7 @@ var circleA = BODY.append('circle')
     .attr('stroke', '#0f5a80');
 
 
-var center = 1;
+
 rectnumber_center = 11;
 var x = 0;
 var y = -50;
@@ -25,14 +25,48 @@ var yl = 50;
 
 
 for ( i = 0; i < rectnumber_center; i++) {
-    x = x + 50;
-    y = y + 50;
-    RECT = BODY.append('rect')
-        .attr('x', x)
-        .attr('y', y)
-        .attr('width', 50)
-        .attr('height', 50)
-        .attr('fill', 'green');
+    var center = 1;
+       if (center === 1){
+            //rectA
+            console.log(center);
+           x = x + 50;
+           y = y + 50;
+           RECT = BODY.append('rect')
+               .attr('x', x)
+               .attr('y', y)
+               .attr('width', 50)
+               .attr('height', 50)
+            .attr('fill', 'green');
+            center = center +1;
+       }
+       else if (center === 2){
+       //rectB
+           console.log(center);
+           //x = x + 50;
+           //y = y + 50;
+           //RECT = BODY.append('rect')
+           //    .attr('x', x)
+           //    .attr('y', y)
+           //    .attr('width', 50)
+           //    .attr('height', 50)
+           //.attr('fill', '#53800c');
+           //center = center +1;
+       }
+       else{
+           //rectC
+           console.log(center);
+          // x = x + 50;
+          // y = y + 50;
+           //RECT = BODY.append('rect')
+           //    .attr('x', x)
+           //    .attr('y', y)
+           //    .attr('width', 50)
+           //    .attr('height', 50)
+          // .attr('fill', '#178067');
+          // center =1;
+       }
+
+
 }
 
 //RIGHT RECT DIAGONAL
